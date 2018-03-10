48649d5da8581f621cec2df94fd179de71dc7c71
Minh <votrannhatminh1508@gmail.com>
2018-03-10 22:33:28 +0700

    Add a comment to explain for a solution

--- src/senseInfosys/com/main/SumToNumberIsArrayEqualK.java
+++ src/senseInfosys/com/main/SumToNumberIsArrayEqualK.java
@@ -8,6 +8,13 @@ public class SumToNumberIsArrayEqualK {
 	// Neu cap so la  5,7 hay 7,5 -> tinh la 1 cap.
 	// VD: Arr = int[] arr = {6,6,6,5,7,7,5,6,6,5,5,7,7,9,3,9,6,5,7,3,3,9}; --> se co cap [6,6] [5,7] [9,3] = 3 pairs hop le
 	
+	/*
+	Solution
+	  1. Run 2 Loop i and j at the same time. Find out pairs have sum is K. 
+	  2. Put those pairs to noDuplicateNums, check whether the pairs is kind of similar with existed pairs.s
+	  3. Return size of noDuplicateNums.  
+	*
+	*/
 	public int manyDistinctPairsHave(int[] numbers, int k) {
 		ArrayList<ArrayList<Integer>> noDuplicateNums = new ArrayList<ArrayList<Integer>>();
 		for (int i = 0; i < numbers.length -1; i++) {
